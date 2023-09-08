2abcb34b375352e7a09d2727a7d11d2ad64c6dbf
Mohammed <maarafimohammed@gmail.com>
2023-09-08 08:46:56 -0700

    alo

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 /**
- * main-function declaration
- *
- *Description: a c program that print a simple string
- *
- *Return: always 0 (success)
-*/
+  * main-function declaration
+  *
+  *Description: a c program that print a simple string
+  *
+  *Return: always 0 (success)
+ */
 int main(void)
 {
 char a;
-for (a = 'a' ; a <= 'z' ; ++a){
+for (a = 'a' ; a <= 'z' ; ++a)
+	{
 	if(a == 'q'|| a == 'e'){
 	continue;
 	}
 	putchar(a);
-}
+	}
 putchar('\n');
 return (0);
 }
