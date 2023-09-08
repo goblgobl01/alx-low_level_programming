9f6e41d0bd003de4b7256d8660b85153726dd622
Mohammed <maarafimohammed@gmail.com>
2023-09-07 23:07:01 -0700

    alo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 /*this a simple program that print a string*/
 #include <stdio.h>
 
+/*this is the main function*/
 int main(void)
 {
 char str[] = "\"Programming is like building a multilingual puzzle";
