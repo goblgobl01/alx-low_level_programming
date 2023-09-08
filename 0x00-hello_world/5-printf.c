fa36695a8798d1fdbfc740cad975dc869287a606
Mohammed <maarafimohammed@gmail.com>
2023-09-08 02:45:08 -0700

    alo

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-function declaration
+ *
+ *Description: a c program that print a simple string also
+ *
+ *Return: always 0 (success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
