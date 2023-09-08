e779e765797f9908a905e83afddea91af629d2c7
Mohammed <maarafimohammed@gmail.com>
2023-09-08 10:07:57 -0700

    alo

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-function declaration
+ *
+ *Description: a c program that print a simple string
+ *
+ *Return: always 0 (success)
+*/
+int main(void)
+{
+int a;
+for ( a = 0 ; a < 10 ; a++ )
+	putchar(a + 48);
+for ( a = 0 ; a < 5 ; a++ )
+	putchar(a + 100);
+putchar('\n');
+
+return (0);
+}
