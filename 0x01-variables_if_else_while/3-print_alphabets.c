9b54bfd92cf0f91d4df91b97a75669901294a7fa
Mohammed <maarafimohammed@gmail.com>
2023-09-08 08:21:07 -0700

    alo

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,11 @@ int main(void)
 { 
 char a;
 char b;
+
 for (a = 'a' ; a <= 'z' ; a++)
-        putchar(a);
+	putchar(a);
 for (b = 'A' ; b <= 'Z' ; b++)
-        putchar(b);
+	putchar(b);
 putchar('\n');
 return (0);
 }
