ccde1781879c9b91b9dff10dafd3e726be098faa
Mohammed <maarafimohammed@gmail.com>
2023-09-08 08:42:22 -0700

    alo

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-function declaration
+ *
+ *Description: a c program that print a simple string
+ *
+ *Return: always 0 (success)
+*/
+int main(void)
+{
+char a;
+for (a = 'a' ; a <= 'z' ; ++a){
+	if(a = 'q'||a = 'e'){
+	continue;
+	}
+	putchar(a);
+}
+putchar('\n');
+return (0);
+}
