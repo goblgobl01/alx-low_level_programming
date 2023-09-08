30f7938d500abeff91505bd6ae75f324862f6600
Mohammed <maarafimohammed@gmail.com>
2023-09-08 08:05:29 -0700

    alo

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-function declaration
+ *
+ *Description: a c program that print a simple string
+ *
+ *Return: always 0 (success)
+*/
+int main(void)
+{
+char a;
+for (a='a';a<='z';a++)
+	putchar(a);
+return (0);
+}
