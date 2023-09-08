b386e6e709e3b59e15638ac3555d8454aa8ccca7
Mohammed <maarafimohammed@gmail.com>
2023-09-08 09:30:20 -0700

    alo

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include  <stdio.h>
+/**
+ * main-function declaration
+ *
+ *Description: a c program that print a simple string
+ *
+ *Return: always 0 (success)
+*/
+
+int main(void)
+{
+char a;
+for(a = 'z' ; a => 'a' ; a--)
+	putchar(a);
+putchar('\n');
+return (o);
+}
