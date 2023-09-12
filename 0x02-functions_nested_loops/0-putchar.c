8fbfb171d4a6281e7bf3a7aeb0a3887cc23f2e1e
Mohammed <maarafimohammed@gmail.com>
2023-09-12 11:31:05 -0700

    alo

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-function declaration
+ *
+ *Description: a c program that print a simple string
+ *
+ *Return: always 0 (success)
+*/
+int main(void)
+{
+printf("_putchar\n")
+}
