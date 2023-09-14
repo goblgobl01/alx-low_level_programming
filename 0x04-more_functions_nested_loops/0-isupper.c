268d1069e187bc10e0160b8954f3c851360a377a
Mohammed <maarafimohammed@gmail.com>
2023-09-14 11:09:07 -0700

    alo

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main-function declaration
+ *
+ *Description: a function that check if a letter is upper case
+ *
+ *Return: always 0 (success)
+*/
+int _isupper(int c) {
+	return (c >= 'A' && c <= 'Z');
+}
