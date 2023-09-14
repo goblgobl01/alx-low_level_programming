12eb3530b652a307fff12496dd6430f60d06715e
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-09-14 22:10:04 +0100

    alo

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,14 @@
 /**
  * print_numbers - this function print numbers from 0 to 9
  *
- * @i: this the operator that increment every time we print a number
  *
  * Return: void function wont  return anything
 */
 void print_numbers(void)
 {
-	int i=0;
+	int c;
 
-	for(i = 0 ; i < 10; i++)
-		_putchar(i+48);
+	for (c = 0 ; i < 10; c++)
+		_putchar (c + 48);
 	_putchar('\n');
 }
-
