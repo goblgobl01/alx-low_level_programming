937c03db4787876309bc01aff5a0539df661b502
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-09-15 22:23:40 +0100

    alo

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,15 +9,16 @@ int i, j;
 if (n >= 1)
 {
 	for (i = 1 ; i <= n ; i++)
-		{
+	{
 		for (j = 1 ; j <= n ; j++)
 		{
 			if (i == j)
 				_putchar('\\');
 			else
-				_putchar(' ')
+				_putchar(' ');
 		}
 	_putchar('\n');
+	}
 }
 else
 	_putchar('\n');
