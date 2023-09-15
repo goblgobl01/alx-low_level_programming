84ad57a4468a99662d20523ee3ed70ec25aeab4b
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-09-15 22:47:09 +0100

    alo

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,7 +7,10 @@
 void print_square(int size)
 {
 int i, j;
-
+if (size <= 0)
+	_putchar('\n');
+else
+{
 for (i = 0 ; i < size ; i++)
 {
 	for (j = 0 ; j < size ; j++)
@@ -15,5 +18,6 @@ for (i = 0 ; i < size ; i++)
 		_putchar('#');
 	}
 _putchar('\n');
+}
 }
 }
