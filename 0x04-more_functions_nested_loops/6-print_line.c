d148338b4f4833f2d02a8b7f8c9abc28ff16a306
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-09-15 21:24:00 +0100

    alo

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,14 +6,19 @@
 
 void print_line(int n)
 {
+
 int i;
-for (i = 0 ; i < n ; i++)
-	if (n >= 0)
-	_putchar('_');
-	else
+
+if (n >= 0)
+{
+	for (i = 0 ; i < n ; i++)
 	{
-	_putchar('\n');
-	break;
+		putchar('_');
 	}
-_putchar('\n');
+	putchar('\n');
+	}
+
+else
+	putchar('\n');
+
 }
