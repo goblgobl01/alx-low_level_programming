ea2fe6cfa9030bcad907110f645b2cfdb1fb9418
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-09-18 15:32:05 +0100

    alo

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - a function that alter a number value
+ *@n : is the pointer to the variable n
+ *Return: always 0 (success)
+*/
+
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
