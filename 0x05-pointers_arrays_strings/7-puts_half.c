a789aa8690199565cba4d2e43c3dcf7982f63970
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-09-18 23:30:55 +0100

    alo

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - Prits half of the string
+ * @str: The input string.
+ */
+void puts_half(char *str)
+{
+
+int moha, i, j;
+
+moha = 0;
+
+while (str[moha] != '\0')
+	moha++;
+if ((moha % 2) == 0)
+{
+	for (i = (moha / 2) ; i <= moha ; i++)
+		_putchar(str[i]);
+}
+else if ((moha % 2) != 0)
+{
+
+	for (j = ((moha - 1) / 2) ; j <= moha ; j++)
+		_putchar(str[j]);
+}
+
+_putchar('\n');
+}
+
