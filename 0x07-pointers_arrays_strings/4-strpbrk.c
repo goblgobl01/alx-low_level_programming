1360448f7fd07e8a69914f7387eab949ea397181
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-09-25 21:14:28 +0100

    alo

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,21 +10,16 @@
 char *_strpbrk(char *s, char *accept)
 {
 int i, j;
-char *First_charcter;
 
 for (i = 0 ; s[i] != '\0' ; i++)
 {
 	for (j = 0 ; accept[j] != '\0' ; j++)
 	{
 	if (s[i] == accept[j])
-		{
-		First_charcter = &s[i];
-		goto exitloops;
-		}
+		return (&s[i]);
 	}
 }
-exitloops:
 
 
-return (First_charcter);
+return ('\0');
 }
