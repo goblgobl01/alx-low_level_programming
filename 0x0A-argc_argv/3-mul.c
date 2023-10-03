72091ae548da146ab66e3b20ac74d1e03a6ef8cb
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-10-03 03:34:45 +0100

    alo

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point of the programme
+ *
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ *
+ * Return: 0 if the program executed successfully, 1 if there was an error.
+ */
+int main(int argc, char *argv[])
+{
+int num2,num1;
+int result;
+
+if (argc != 3)
+{
+printf("Error: Please provide exactly two integer arguments.\n");
+return (1);
+}
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 * num2;
+
+printf("%d\n", result);
+
+return (0);
+}
