239f39f7e9d5df9929b3df3770ff04a7d468f156
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-10-06 19:49:39 +0100

    alo

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,4 @@
-#inlucde <stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point of the program
  *
@@ -9,37 +9,49 @@
  */
 int main(int argc, char **argv)
 {
-int i, j;
-int result1, result2;
-int result;
-int sign1 = 1;
-int sign2 = 1;
+int i, j, result1, result2, result, sign1, sign2;
 (void) argc;
+
+sign1 = 1;
+sign2 = 1;
 
 if (argv[1] == NULL || argv[2] == NULL)
 {
-	printf ("Error\n");
+	printf("Error\n");
 	return (1);
 }
 
 else
 {
-i=0;
+if (argv[1][0] == '-')
+	sign1 = 0;
+
+i = 0;
 while (argv[1][i])
 {
 
-	if(argv[1][i] >= '0' && argv[1][i]  <= '9')
-		result1 = result1 *10 + (argv[1][i] - '0');
+	if (argv[1][i] >= '0' && argv[1][i]  <= '9')
+		result1 = result1 * 10 + (argv[1][i] - '0');
 	i++;
 }
 
-j=0;
+if (sign1 == 0)
+	result1 = -result1;
+
+
+if (argv[2][0] == '-')
+	sign2 = 0;
+
+j = 0;
 while (argv[2][j])
 {
-	if(argv[2][j] >= '0' && argv[2][j]  <= '9')
-		result2 = result2 *10 + (argv[2][j] - '0');
+	if (argv[2][j] >= '0' && argv[2][j]  <= '9')
+		result2 = result2 * 10 + (argv[2][j] - '0');
 	j++;
 }
+
+if (sign2 == 0)
+	result2 = -result2;
 
 result = result1 * result2;
 
