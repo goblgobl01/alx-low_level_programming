ef9b1f05b192079a84ca263f8a146319e992711c
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-10-06 19:28:41 +0100

    alo

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
-
-
+#inlucde <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
 int main(int argc, char **argv)
 {
 int i, j;
 int result1, result2;
 int result;
+int sign1 = 1;
+int sign2 = 1;
 (void) argc;
 
 if (argv[1] == NULL || argv[2] == NULL)
@@ -19,6 +27,7 @@ else
 i=0;
 while (argv[1][i])
 {
+
 	if(argv[1][i] >= '0' && argv[1][i]  <= '9')
 		result1 = result1 *10 + (argv[1][i] - '0');
 	i++;
