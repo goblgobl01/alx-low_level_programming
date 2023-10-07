a06ffec1220e279ac6fed5d97e9fa2e25973639e
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-10-07 02:56:06 +0100

    alo

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
+/**
+ * main - multiplicatiion of two parameters
+ *
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Return: 1 if error 0 if success
+ */
 int main(int argc, char **argv)
 {
 int i, result, j;
@@ -14,24 +21,25 @@ if (argc == 1)
 
 else
 {
-i = 1;
-result = 0;
-while (i < argc)
-{
-	j = 0;
-	while (argv[i])
+	i = 1;
+	result = 0;
+
+	while (i < argc)
 	{
-		if (argv[i][j] < '0' || argv[i][j] > '9')
+		j = 0;
+		while (argv[i][j])
 		{
-			printf("Error\n");
-			return (1);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
 		}
-		j++;
-	}
 	result = result + atoi(argv[i]);
 	i++;
-}
-printf ("%d\n", result);
+	}
+printf("%d\n", result);
 }
 return (0);
 }
