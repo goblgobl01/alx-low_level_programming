7bbe06fac20853ad928cf476ca84fa5a41dfbe43
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-10-08 07:08:17 +0100

    alo

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,28 +1,30 @@
 #include <stdlib.h>
-#include <string.h>
 #include "main.h"
+
 /**
- * _calloc - Allocates memory for an array using malloc.
+ * *array_range - creates an array of integers
+ * @min: minimum range of values stored
+ * @max: maximum range of values stored and number of elements
  *
- * @nmemb: The number of elements in the array.
- * @size: The size (in bytes) of each element.
- *
- * Return: If nmemb or size is 0, or if malloc fails, returns NULL.
- *         Otherwise, returns a pointer to the allocated memory.
+ * Return: pointer to the new array
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+int *array_range(int min, int max)
 {
-void *ptr;
+	int *ptr;
+	int i, size;
 
-if (nmemb == 0 || size == 0)
-	return (NULL);
+	if (min > max)
+		return (NULL);
 
-ptr = malloc(nmemb * size);
+	size = max - min + 1;
 
-if (ptr == NULL)
-	return (NULL);
+	ptr = malloc(sizeof(int) * size);
 
-memset(ptr, 0, (nmemb * size));
+	if (ptr == NULL)
+		return (NULL);
 
-return (ptr);
+	for (i = 0; min <= max; i++)
+		ptr[i] = min++;
+
+	return (ptr);
 }
