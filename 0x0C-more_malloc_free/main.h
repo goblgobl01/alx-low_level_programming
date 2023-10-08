9412ffdd2c086e4e275aafe0df3d25f3b997437b
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-10-08 09:48:06 +0100

    alo

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
