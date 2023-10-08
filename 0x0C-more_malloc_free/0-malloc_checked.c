bac39e57cc0cc6bcc10b5ca5699f42693f9e99bc
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-10-08 03:59:46 +0100

    alo

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+
+ptr = malloc (b);
+
+if (ptr == Null)
+	printf("98");
+else
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h> // Include this for the definition of size_t
+
+void *malloc_checked(unsigned int b);
+
+#endif
