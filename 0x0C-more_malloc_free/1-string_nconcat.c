dbe673defc5caea52969d5bb4fafdcd8b12efa99
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-10-08 05:14:53 +0100

    alo

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,7 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 unsigned int a, b;
 char *ptr;
 
-if (n >= sizeof(s2))
+if (n >= strlen(s2))
 {
 	ptr = malloc(strlen(s1) + strlen(s2) + 1);
 }
