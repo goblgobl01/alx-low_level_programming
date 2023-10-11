0df2df0ffa035187e1a2721314ef316d066a0014
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-10-11 10:21:39 +0100

    alo

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,23 +11,15 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+int i;
+
 if (size <= 0)
-	return (-1);
-
-else
+	return -1;
+for (i = 0; i < size; i++)
 {
-	int i, result;
-
-	i = 0;
-	result = 0;
-	while (i < size)
-	{
-		result = cmp(*(array + i));
-		i++;
-		if (result != 0)
-			return (i - 1);
-	}
+	if (cmp(array[i]) != 0)
+	return i;
 }
 
-return (-1);
+return -1;
 }
