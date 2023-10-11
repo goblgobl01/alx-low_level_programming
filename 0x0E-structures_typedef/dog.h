a96657ef6ab8e76d0d9e4c22922fccc697f40f66
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-10-11 01:57:11 +0100

    alo

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef YOUR_HEADER_FILE_NAME_H
 #define YOUR_HEADER_FILE_NAME_H
 
+/**
+ * struct dog - this a struture of a dog information
+ * @age: the age of the dog
+ * @owner: the name of the owner
+ * @name: string of the name of the dog
+ *
+ * Description: this is the description
+ */
 struct dog
 {
 char *name;
