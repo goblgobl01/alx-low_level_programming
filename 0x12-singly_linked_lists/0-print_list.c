cfcd965a93b1fd62ad01b1a966a98f8a937e6d8e
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-10-19 19:44:29 +0100

    alo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,14 +15,14 @@ size_t print_list(const list_t *h)
 int count = 0;
 
 if (h == NULL)
-	return (NULL);
+	return (0);
 
 while (h != NULL)
 {
 	if (h->str != NULL)
 	{
 		printf("[%d] %s\n", h->len, h->str);
-		return (NULL);
+		return (0);
 	}
 	else
 		printf("[0] (nil)\n");
