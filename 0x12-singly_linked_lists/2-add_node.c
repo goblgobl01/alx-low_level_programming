feb7414b6f2f803dda341097db3d7cf05d014e55
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-10-19 17:08:34 +0100

    alo

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,12 +7,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new;
+int len = 0;
+
+while(str[len] != '\0')
+	len++;
 
 new = malloc(sizeof(list_t));
 if(new == NULL)
 	return (NULL);
 
 new->str = strdup(str);
+new->len = len;
 
 new->next = *head;
 *head = new;
