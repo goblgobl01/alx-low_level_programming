9b8cd089cc5c9cdb80707150a4826469894548f5
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-10-19 19:17:10 +0100

    alo

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,7 +3,11 @@
 #include <stdio.h>
 #include "lists.h"
 
-
+/**
+ * free_list - Frees a linked list
+ *
+ * @head: A pointer to the first node in the list.
+ */
 void free_list(list_t *head)
 {
 list_t *temp;
