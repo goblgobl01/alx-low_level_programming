9bc78e5022da35a095fa88ea2a4c7390d5afb524
Mohammed Maarafi <maarafimohammed@gmial.com>
2023-10-19 19:16:03 +0100

    alo

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+void free_list(list_t *head)
+{
+list_t *temp;
+
+while (head != NULL)
+{
+	temp = head;
+	head = head->next;
+	free(temp);
+}
+}
