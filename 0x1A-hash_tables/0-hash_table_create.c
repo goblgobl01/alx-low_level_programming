37ab3d055dc51d4df49a0832e3e8529dba4cb5e7
Mohammed Maarafi <maarafimohammed@gmail.com>
2023-12-22 10:30:22 +0100

    alo

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,8 +6,6 @@ hash_table_t *hash_table_create(unsigned long int size)
     if(table == NULL)
         return (NULL);
     table->size = size;
-    if(table->array == NULL)
-            return (NULL);
 
     return(table);
 }
